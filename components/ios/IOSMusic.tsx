2083ce2da04b84d8fee567de1f5c73bdf88fff1e
Aditya Joshi <159620541+IamKavy47@users.noreply.github.com>
2025-03-15 07:35:46 +0530

    Update IOSMusic.tsx

--- components/ios/IOSMusic.tsx
+++ components/ios/IOSMusic.tsx
@@ -849,7 +849,7 @@ export default function IOSMusic({ onClose }: IOSMusicProps) {
   )
 
   return (
-    <div className="h-[812px] w-[375px] bg-black text-white flex flex-col overflow-y-auto">
+    <div className="h-[800px] w-[363px] bg-black text-white flex flex-col overflow-y-auto">
       {/* Main content */}
       <AnimatePresence mode="wait">
         {activeView === "home" && (
