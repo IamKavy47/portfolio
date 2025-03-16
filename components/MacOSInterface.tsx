6c2018428e90870f3ba6a254e0080795dffad2e1
Aditya Joshi <159620541+IamKavy47@users.noreply.github.com>
2025-03-16 20:45:55 +0530

    Update MacOSInterface.tsx

--- components/MacOSInterface.tsx
+++ components/MacOSInterface.tsx
@@ -18,7 +18,7 @@ export default function MacOSInterface() {
   const [openApps, setOpenApps] = useState<string[]>([])
   const [activeApp, setActiveApp] = useState<string | null>(null)
   const [wallpaper, setWallpaper] = useState(
-    "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-Ww1DQ6z0nNFu7dGuWaC8MLVOWnQDOQ.png",
+    "iamkavy47.vercel.app/wall/wall1.jpg",
   )
 
   const toggleApp = (appName: string) => {
